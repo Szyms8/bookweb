cfd6a532218cc6271b6145cefab24db20c16608e
Szymon Wojciechowski <wojszymil@gmail.pl>
2024-06-29 16:23:19 +0200

    correct menu bars and x

--- js/script.js
+++ js/script.js
@@ -1,145 +1,70 @@
-// alert("bubba")
-let todoInput,
-	errorInfo,
-	addBtn,
-	ulList,
-	newTodo,
-	popupV,
-	popupInfo,
-	todoToEdit,
-	popupInput,
-	popupAddBtn,
-	popupcloseBtn;
 
-const main = () => {
-	prepareDOMElements();
-	prepareDOMEvents();
-};
 
-const prepareDOMElements = () => {
-	// pobiera wszystkie nasze el.
-	todoInput = document.querySelector(".todo-input");
-	errorInfo = document.querySelector(".error-info");
-	addBtn = document.querySelector(".btn-add");
-	ulList = document.querySelector(".todolist ul");
 
-	popupV = document.querySelector(".popup");
-	popupInfo = document.querySelector(".popup-info");
-	popupInput = document.querySelector(".popup-input");
-	popupAddBtn = document.querySelector(".accept");
-	popupcloseBtn = document.querySelector(".cancel");
-};
-
-const prepareDOMEvents = () => {
-	//nadajemy nasłuchiwanie
-	addBtn.addEventListener("click", addNewTodo);
-	ulList.addEventListener("click", checkClick);
-	popupcloseBtn.addEventListener("click", closePopupFunction);
-	popupAddBtn.addEventListener("click", changeTodoText);
-	todoInput.addEventListener("keyup", enterKeyCheck);
-};
-
-const addNewTodo = () => {
-	if (todoInput.value !== "") {
-		newTodo = document.createElement("li");
-		newTodo.textContent = todoInput.value;
-		createToolsArea();
-		ulList.append(newTodo);
-
-		todoInput.value = "";
-		errorInfo.textContent = "";
-	} else {
-		errorInfo.textContent = "Enter a book title";
-	}
-};
-
-const createToolsArea = () => {
-	const toolsPanel = document.createElement("div");
-	toolsPanel.classList.add("tools");
-	// newTodo.append(toolsPanel);
-
-	const completeBtn = document.createElement("button");
-	completeBtn.classList.add("complete");
-	completeBtn.innerHTML = `<i class="fas fa-check"></i>`;
-
-	const editBtn = document.createElement("button");
-	editBtn.classList.add("edit");
-	editBtn.textContent = "EDIT";
-
-	const deleteBtn = document.createElement("button");
-	deleteBtn.classList.add("delete");
-	deleteBtn.innerHTML = `<i class="fas fa-times"></i>`;
-
-	toolsPanel.append(completeBtn, editBtn, deleteBtn);
-
-	newTodo.append(toolsPanel);
-};
-
-const checkClick = (e) => {
-	if (e.target.matches(".complete")) {
-		e.target.closest("li").classList.toggle("completed");
-		e.target.classList.toggle("completed");
-	} else if (e.target.matches(".edit")) {
-		editTodo(e);
-	} else if (e.target.matches(".delete")) {
-		deleteTodo(e);
-	}
-};
-
-const editTodo = (e) => {
-	todoToEdit = e.target.closest("li");
-	popupInput.value = todoToEdit.firstChild.textContent;
-	console.log(todoToEdit.firstChild);
-	popupV.style.display = "flex";
-};
-
-const closePopupFunction = () => {
-	popupV.style.display = "none";
-	popupInfo.textContent = "";
-};
-
-const changeTodoText = () => {
-	if (popupInput.value !== "") {
-		todoToEdit.firstChild.textContent = popupInput.value;
-		popupV.style.display = "none";
-		popupInfo.textContent = "";
-	} else {
-		popupInfo.textContent = "Musisz podać jakąś treść";
-	}
-};
-
-const deleteTodo = (e) => {
-	e.target.closest("li").remove();
-
-	const allTodos = ulList.querySelectorAll("li");
-
-	if (allTodos.length === 0) {
-		errorInfo.textContent = "No boooks on the list";
-	}
-};
-
-const enterKeyCheck = (e) => {
-	if (e.key === "Enter") {
-		addNewTodo();
-	}
-};
-
-document.addEventListener("DOMContentLoaded", main);
-const navbarV = document.getElementsByClassName("navbar-toggle")[0];
-const navbarV2 = document.getElementsByClassName("navbar-toggle2")[0];
-const navbarLinksV = document.getElementsByClassName("navbar-links");
+// const navbarV = document.getElementsByClassName("navbar-toggle");
+const navbarV = document.querySelector(".navbar-toggle");
+// const navbarV = document.querySelector(".navbar-toggle");
+const navI = document.querySelector(".fa-bars");
+const navbarV2 = document.querySelector(".navbar-toggle2");
+// const navbarLinksV = document.getElementsByClassName("navbar-links");
+const navbarLinksV = document.querySelectorAll(".navbar-links");
 const scrollLBtn = document.querySelector(".scrolll");
 const scrollRBtn = document.querySelector(".scrollr");
 const cardsBtnL = document.querySelector(".scrolll-recomended-left");
 const cardsBtnR = document.querySelector(".scrollr-recomended-right");
 
-const functionShow = () => {
+const hamburger = document.querySelector(".navbar-links");
+const navMenu = document.querySelector(".nav-menuu");
+
+// const functionShow = () => {
+// 	for (let i = 0; i < navbarLinksV.length; i++) {
+// 		navbarLinksV[i].classList.toggle("active");
+// 		navbarV.classList.toggle(".none");
+// 		navbarV2.classList.toggle(".none");
+// 	}
+
+	// const functionClose = () => {
+	// 	for (let i = 0; i < navbarLinksV.length; i++) {
+	// 		navbarLinksV[i].classList.remove("active");
+	// 		// navbarV.classList.toggle("none");
+	// 		// navbarV2.classList.toggle("none");
+	// 	}
+	// };
+// 	// navbarLinksV.addEventListener("click", functionClose);
+// };
+
+// navbarV.addEventListener("click", () => {
+// 	navbarLinksV.classList.toggle("active")
+// 	// navMenu.classList.toggle("active")
+// })
+
+// document.querySelectorAll(".navbar-links").forEach(x => x.addEventListener("click", () => {
+// 	navbarLinksV.remove("active");
+
+// }))
+
+
+
+function fShow() {
 	for (let i = 0; i < navbarLinksV.length; i++) {
 		navbarLinksV[i].classList.toggle("active");
-		navbarV.classList.toggle("none");
-		navbarV2.classList.toggle("none");
 	}
-};
+	navI.classList.toggle("none");
+	navbarV2.classList.toggle("none");
+	// navbarLinksV.classList.toggle("active")
+
+	// navbarV.addEventListener("click", () => {
+	// 	navbarLinksV.classList.toggle("active")
+	// 	// navMenu.classList.toggle("active")
+	// })
+}
+function fClose() {
+	for (let i = 0; i < navbarLinksV.length; i++) {
+		navbarLinksV[i].classList.toggle("active");
+		}
+		navI.classList.toggle("none");
+		navbarV2.classList.toggle("none");
+}
 
 function fscrollL() {
 	let left = document.querySelector(".scroll-images");
@@ -161,9 +86,14 @@ function fscrollLCardsR() {
 	// console.log("dipa");
 }
 
-navbarV.addEventListener("click", functionShow);
-navbarV2.addEventListener("click", functionShow);
+// navbarV.addEventListener("click", functionShow);
+// navbarV2.addEventListener("click", functionShow);
 scrollLBtn.addEventListener("click", fscrollL);
 scrollRBtn.addEventListener("click", fscrollR);
 cardsBtnL.addEventListener("click", fscrollLCardsL);
 cardsBtnR.addEventListener("click", fscrollLCardsR);
+navbarV.addEventListener("click", fShow);
+navbarV2.addEventListener("click", fClose);
+// navI.addEventListener("click", fClose);
+// navbarLinksV.addEventListener("click", functionCloseNav)
+
